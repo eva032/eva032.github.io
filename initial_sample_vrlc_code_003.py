e5e7d65dbf08fd68e5c2704568e51aa36ba1ecd6
eva032 <110508134+eva032@users.noreply.github.com>
2022-08-19 16:15:13 -0700

    Update initial_sample_vrlc_code_003.py

--- initial_sample_vrlc_code_003.py
+++ initial_sample_vrlc_code_003.py
@@ -9,63 +9,63 @@ vrlc.a_entity("a-entity", "c1","e4","raycaster~far:30;objects:.intersectable,cur
 
 #vrlc.CreateEntity(mixInObj, OuterObj, InnerObj, jsEvt_Click, jsEvt_MouseEnter)
 #creating 12 boxes to represent 12 keys
-vrlc.CreateEntity("geometry~primitive:box,material~color:pink",
-					"position~-9 0 -5",
+vrlc.CreateEntity("geometry~primitive:box,material~color:white",
+					"position~-7.5 0 -5",
 					"class~intersectable",
 					"jsPlayNote('C')",
 					"jsPlayNote('C')")
-vrlc.CreateEntity("geometry~primitive:box,material~color:pink",
-					"position~-7.5 0 -6",
+vrlc.CreateEntity("geometry~primitive:box,material~color:blue",
+					"position~-6 0 -6",
 					"class~intersectable",
 					"jsPlayNote('C#')",
 					"jsPlayNote('C#')")
 
-vrlc.CreateEntity("geometry~primitive:box,material~color:pink",
-					"position~-6 0 -5",
+vrlc.CreateEntity("geometry~primitive:box,material~color:white",
+					"position~-4.5 0 -5",
 					"class~intersectable",
 					"jsPlayNote('D')",
 					"jsPlayNote('D')")
-vrlc.CreateEntity("geometry~primitive:box,material~color:pink",
-					"position~-4.5 0 -6",
+vrlc.CreateEntity("geometry~primitive:box,material~color:blue",
+					"position~-3 0 -6",
 					"class~intersectable",
 					"jsPlayNote('D#')",
 					"jsPlayNote('D#')")
-vrlc.CreateEntity("geometry~primitive:box,material~color:pink",
-					"position~-3 0 -5",
+vrlc.CreateEntity("geometry~primitive:box,material~color:white",
+					"position~-1.5 0 -5",
 					"class~intersectable",
 					"jsPlayNote('E')",
 					"jsPlayNote('E')")
-vrlc.CreateEntity("geometry~primitive:box,material~color:pink",
+vrlc.CreateEntity("geometry~primitive:box,material~color:white",
 					"position~0 0 -5",
 					"class~intersectable",
 					"jsPlayNote('F')",
 					"jsPlayNote('F')")
-vrlc.CreateEntity("geometry~primitive:box,material~color:pink",
+vrlc.CreateEntity("geometry~primitive:box,material~color:blue",
 					"position~1.5 0 -6",
 					"class~intersectable",
 					"jsPlayNote('F#')",
 					"jsPlayNote('F#')")
-vrlc.CreateEntity("geometry~primitive:box,material~color:pink",
+vrlc.CreateEntity("geometry~primitive:box,material~color:white",
 					"position~3 0 -5",
 					"class~intersectable",
 					"jsPlayNote('G')",
 					"jsPlayNote('G')")
-vrlc.CreateEntity("geometry~primitive:box,material~color:pink",
+vrlc.CreateEntity("geometry~primitive:box,material~color:blue",
 					"position~4.5 0 -6",
 					"class~intersectable",
 					"jsPlayNote('G#')",
 					"jsPlayNote('G#')")
-vrlc.CreateEntity("geometry~primitive:box,material~color:pink",
+vrlc.CreateEntity("geometry~primitive:box,material~color:white",
 					"position~6 0 -5",
 					"class~intersectable",
 					"jsPlayNote('A')",
 					"jsPlayNote('A')")
-vrlc.CreateEntity("geometry~primitive:box,material~color:pink",
+vrlc.CreateEntity("geometry~primitive:box,material~color:blue",
 					"position~7.5 0 -6",
 					"class~intersectable",
 					"jsPlayNote('A#')",
 					"jsPlayNote('A#')")
-vrlc.CreateEntity("geometry~primitive:box,material~color:pink",
+vrlc.CreateEntity("geometry~primitive:box,material~color:white",
 					"position~9 0 -5",
 					"class~intersectable",
 					"jsPlayNote('B')",
