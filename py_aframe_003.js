b10ea21574985e1910951e7e67d89072cf2264c1
eva032 <110508134+eva032@users.noreply.github.com>
2022-08-21 11:39:27 -0700

    Update py_aframe_003.js

--- py_aframe_003.js
+++ py_aframe_003.js
@@ -10,37 +10,37 @@ function jsPlayNote(note) {
     if (note == "C") {
         oscillator.frequency.value = 261.63;
     }
-    if (note == "C#") {
+    elif (note == "C#") {
         oscillator.frequency.value = 277.18;
     }
-    if (note == "D") {
+    elif (note == "D") {
         oscillator.frequency.value = 293.66;
     }
-    if (note == "D#") {
+    elif (note == "D#") {
         oscillator.frequency.value = 311.13;
     }
-    if (note == "E") {
+    elif (note == "E") {
         oscillator.frequency.value = 329.63;
     }
-    if (note == "F") {
+    elif (note == "F") {
         oscillator.frequency.value = 349.23;
     }
-    if (note == "F#") {
+    elif (note == "F#") {
         oscillator.frequency.value = 369.99;
     }
-    if (note == "G") {
+    elif (note == "G") {
         oscillator.frequency.value = 392;
     }
-    if (note == "G#") {
+    elif (note == "G#") {
         oscillator.frequency.value = 415.30;
     }
-    if (note == "A") {
+    elif (note == "A") {
         oscillator.frequency.value = 440;
     }
-    if (note == "A#") {
+    elif (note == "A#") {
         oscillator.frequency.value = 466.16;
     }
-    if (note == "B") {
+    elif (note == "B") {
         oscillator.frequency.value = 493.88;
     }
     oscillator.connect(context.destination);
